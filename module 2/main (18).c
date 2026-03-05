4288f2d86028bdfaac8eb1c9d2908060c753d47f
jadejayogendrasinh05-max <jadejayogendrasinh05@gmail.com>
2026-03-05 16:38:12 +0530

    commit

---
+++ module 2/main (18).c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    
+    int marks;
+    
+    printf("enter your marks");
+    scanf("%d",&marks);
+    
+    if(marks>=35)
+    {
+        printf("you are pass");
+        
+        
+    }
+
+else
+{
+    
+    printf("you are fail");
+    
+}
+
+getch();
+
+}
---
+++ module 2/main (22).c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    
+    int pizza = 100;
+    int burger = 70;
+    int coffee = 120;
+    int total = 0;
+    
+    total+=pizza;
+    total+=burger;
+    total+=coffee;
+    total+=pizza;
+    total+=burger;
+    total+=coffee;
+    printf("total is %d",total);
+    
+    if(total>500)
+    {
+        printf("discount applied");
+        
+    }
+    else
+    {
+        printf("no discount");
+        
+    }
+    
+    
+    return 0;
+}
