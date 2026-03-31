845929738bc423bfad120b9d1360d79d4bb193f3
jadejayogendrasinh05-max <jadejayogendrasinh05@gmail.com>
2026-03-31 15:25:00 +0530

    commit

---
+++ module 2/main (87).c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+#include<string.h>
+int main()
+{
+    
+    char ch[50]="mango";
+    char ch2[50];
+    do
+    {
+        printf("what is your fav. fruit?");
+        scanf("%s",&ch2);
+        
+    }
+    while(strcmp(ch,ch2)!=0);
+    
+    printf("answer is correct");
+    
+    return 0;
+}
