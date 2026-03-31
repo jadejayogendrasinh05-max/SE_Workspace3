9d64b21b19b1b026b1f60ea2cfa2284711ad25ce
jadejayogendrasinh05-max <jadejayogendrasinh05@gmail.com>
2026-03-31 16:06:20 +0530

    commit

---
+++ module 2/main (98).c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+
+
+//with parameter without return type
+void add(int a,int b)
+{
+    
+    int c = a+b;
+    printf("%d",c);
+    
+}
+
+int main()
+{
+    add(6,3);
+    
+    return 0;
+    
+}
